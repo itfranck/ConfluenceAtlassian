234369e219d59af40dbf0625426cb955ff758637
Francis Mercier <Francis@nzt.ca>
2021-05-25 20:45:16 -0400

    New-CaSpace / Remove-CASpace

---
+++ Public/New-CASpace.ps1
@@ -0,0 +1,41 @@
+﻿function New-CASpace {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ $_.length -le 200})]
+        [String]$Name, 
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( { $_ -match '^\w{1,255}$' })]
+        [String]$Key, 
+        [String]$Description = '',
+        [Switch]$Private
+    )
+
+    $PrivateSuffix = ''
+    if ($Private) { $PrivateSuffix = '/_private' }
+
+    $Body = @{
+        key         = $Key
+        name        = $Name
+        description = @{'plain' = $Description }
+    } | ConvertTo-Json
+
+    try {
+        $Result = Invoke-CAApi -Method Post -Path "/space$PrivateSuffix" -Body $Body -ContentType application/json  -ErrorAction Stop
+        return New-Object -TypeName CASpace -Property @{
+            Id          = $Result.id
+            Key         = $Result.key
+            Name        = $Result.name
+            Type        = $Result.type
+            Status      = $Result.status
+            BaseObject  = $Result
+            Description = $Result.description.plain.value
+            Url         = "$($Script:CAContext.BaseUrl)$($Result._links.webui)"
+        }
+    }
+    catch {
+        Write-Error $_
+    }
+
+}
---
+++ Public/Remove-CASpace.ps1
@@ -0,0 +1,17 @@
+﻿function Remove-CASpace {
+    [CmdletBinding()]
+    param (
+        [ValidateNotNull()]
+        [ValidateScript( { ![String]::IsNullOrWhiteSpace($_.Key) })]
+        [Parameter(ParameterSetName = 'BySpace', Mandatory = $true)]
+        [CASpace]$Space,
+        [Parameter(ParameterSetName = 'ByKey', Mandatory = $true)]
+        [String]$Key
+
+    )
+
+    
+    if ($PSCmdlet.ParameterSetName -eq 'BySpace') {$Key = $Space.Key}
+      Invoke-CAApi -Method Delete -Path "/space/$Key" -Body $Body -ContentType application/json 
+
+}
